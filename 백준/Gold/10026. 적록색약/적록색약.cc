710ea05c28fd3cd30d0ef3c4ab2e9a435efb557d
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2023-08-18 15:35:22 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -9,7 +9,6 @@ int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
 
 void dfs(char color, int y, int x, int n);
-void rgdfs(char color, int y, int x, int n, bool rgflag);
 
 int main(){
 	ios_base::sync_with_stdio(false);
@@ -32,8 +31,7 @@ int main(){
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
 			if(!flag[i][j]){
-				if(c[i][j] == 'R' || c[i][j] == 'G') rgdfs(c[i][j], i, j, n, true);
-				else rgdfs(c[i][j], i, j, n, false);
+				dfs(c[i][j], i, j, n);
 				rgcnt++;
 			}
 		}
@@ -44,19 +42,10 @@ int main(){
 
 void dfs(char color, int y, int x, int n){
 	flag[y][x] = true;
+	if(c[y][x] == 'G') c[y][x] = 'R';
 	for(int i=0;i<4;i++){
 		int ny = y + dy[i], nx = x + dx[i];
 		if(ny<0 || ny>=n || nx<0 || nx>=n || flag[ny][nx] || c[ny][nx] != color) continue;
 		dfs(color, ny, nx, n);
-	}
-}
-
-void rgdfs(char color, int y, int x, int n, bool rgflag){
-	flag[y][x] = true;
-	for(int i=0;i<4;i++){
-		int ny = y + dy[i], nx = x + dx[i];
-		if(ny<0 || ny>=n || nx<0 || nx>=n || flag[ny][nx] 
-			|| (rgflag && c[ny][nx] == 'B') || (!rgflag && (c[ny][nx] == 'R' || c[ny][nx] == 'G'))) continue;
-		rgdfs(color, ny, nx, n, rgflag);
 	}
 }
