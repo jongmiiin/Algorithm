faca723910a573f87f211932fa86f33bd60cfba5
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2023-08-31 17:22:03 +0900

    [Silver III] Title: 후위 표기식2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int alpa[34];
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	for(int i=0;i<n;i++)
+		cin >> alpa[i];
+	stack<double> stk;
+	for(int i=0;i<s.size();i++){
+		if(s[i]>='A' && s[i]<='Z') stk.push(alpa[s[i]-65]);
+		else{
+			double b = stk.top();
+			stk.pop();
+			double a = stk.top();
+			stk.pop();
+			if(s[i] == '+') stk.push(a+b);
+			if(s[i] == '-') stk.push(a-b);
+			if(s[i] == '*') stk.push(a*b);
+			if(s[i] == '/') stk.push(a/b);
+		}
+	}
+	cout << fixed;
+	cout.precision(2);
+	while(!stk.empty()){
+		cout << stk.top() << endl;
+		stk.pop();
+	}
+	return 0;
+}
