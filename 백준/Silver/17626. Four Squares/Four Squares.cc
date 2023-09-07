eeb377f036a10942b7ba9a7c2329ab20fbc1e79a
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2023-09-07 21:43:42 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[50004];
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	for(int i=1;i<=n;i++)
+		for(int j=1;j*j<=i;j++)
+			if(dp[i]>dp[i-j*j]+1 || dp[i] == 0)
+				dp[i]=dp[i-j*j]+1;
+	cout << dp[n];
+	return 0;
+}
