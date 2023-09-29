6618a1b0d4103b0c1bdc6ba046730fa4245b0389
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2023-09-29 16:26:14 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[14], ans[14];
+
+void f(int n, int m, int inx);
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	for(int i=1;i<=n;i++)
+		cin >> arr[i];
+	sort(arr+1, arr+n+1);
+	f(n, m, 1);
+	return 0;
+}
+
+void f(int n, int m, int inx){
+	if(inx == m+1){
+		for(int i=1;i<=m;i++)
+			cout << ans[i] << " ";
+		cout << '\n';
+	}
+	else{
+		int ex = 0;
+		for(int i=1;i<=n;i++){
+			if(ex<arr[i] && ans[inx-1]<=arr[i]){
+				ex = arr[i];
+				ans[inx] = arr[i];
+				f(n, m, inx+1);
+				if(inx==1) ans[0] = arr[i];
+			}
+		}
+	}
+}
