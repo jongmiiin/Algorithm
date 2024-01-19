bf51ca011a8e087656893e80be2945065c324181
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2024-01-19 13:45:59 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	long long sum = 0;
+	for(int i=0;i<5;i++){
+		long long t;
+		cin >> t;
+		sum += t;
+	}
+	cout << sum;
+	return 0;
+}
