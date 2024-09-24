29bca3ae2f157fa107df03f042eb801326d8271e
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2024-09-24 14:38:14 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<int> ans;
+int arr[14];
+void rec(int inx, int lev);
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	while(1){
+		cin >> n;
+		if(n==0) break;
+		for(int i=0;i<n;i++)
+			cin >> arr[i];
+		rec(0, 0);
+		cout << endl;
+	}
+	return 0;
+}
+
+void rec(int inx, int lev){
+	if(lev==6){
+		for(int i=0;i<6;i++)
+			cout << ans[i] << " ";
+		cout << "\n";
+		return;
+	}
+	for(int i=inx;i<n;i++){
+		ans.push_back(arr[i]);
+		rec(i+1, lev+1);
+		ans.erase(ans.end()-1);
+	}
+}
