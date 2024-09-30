3277cf7f008c0194a8772230643515e1307d1bfb
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2024-09-30 15:08:41 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// bandage_len은 배열 bandage의 길이입니다.
+// attacks_rows는 2차원 배열 attacks의 행 길이, attacks_cols는 2차원 배열 attacks의 열 길이입니다.
+int solution(int bandage[], size_t bandage_len, int health, int** attacks, size_t attacks_rows, size_t attacks_cols) {
+    int answer = 0;
+    return answer;
+}
