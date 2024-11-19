2f8fa69c429cae022211df7e725b649761e927b8
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2024-11-19 15:15:56 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,34 +1,23 @@
-vows = ['a','e','i','o','u']
-
-def pos():
-	global ans
-	vow = 0
-	cons = 0
-	for i in ans:
-		if i in vows :
-			vow+=1
-		else:
-			cons+=1
-	if vow>=1 and cons>=2:
-		return True
-	else:
-		return False
-
-def rec(idx, lev):
-	global arr, ans, n, k
-	if lev==n:
-		if pos():
-			for i in ans:
-				print(i, end='')
-			print()
-		return
-	for i in range(idx, k):
-		ans.append(arr[i])
-		rec(i+1, lev+1)
-		ans.pop()
-
+vowels = ['a', 'e', 'i', 'o', 'u']
 ans = []
-n, k = map(int, input().split())
+
+def f(idx, lev):
+	global l, c, ans, vowels, arr
+	if lev == l:
+		cnt = 0
+		for char in ans:
+			if char in vowels:
+				cnt+=1
+		if 0<cnt<l-1:
+			print("".join(ans))
+	else:
+		for i in range(idx, c):
+			ans.append(arr[i])
+			f(i+1, lev+1)
+			ans.pop()
+
+
+l, c = map(int, input().split())
 arr = input().split()
 arr.sort()
-rec(0,0)
+f(0, 0)
