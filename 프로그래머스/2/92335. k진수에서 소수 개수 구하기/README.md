33db2d01fd5fd1c5585b4372ca9db4b54c316e06
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2026-05-13 23:12:40 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 44.19 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335#) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 61.31 ms
+메모리: 9.22 MB, 시간: 44.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 13일 23:10:00
+2026년 05월 13일 23:12:37
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -5,18 +5,8 @@ def k_ary(n, k):
     while n > 0:
         ret+=str(n%k)
         n = n//k
+        
     return ret[::-1]
-
-def prime(n):
-    flag = [True] * (n+1)
-    flag[0] = False
-    flag[1] = False
-    
-    for i in range(2, int(sqrt(n))+1):
-        if flag[i]:
-            for j in range(i*2, n+1, i):
-                flag[j] = False
-    return flag
 
 def is_prime(x):
     if x < 2:
@@ -32,8 +22,6 @@ def solution(n, k):
     answer = 0
     k_num = k_ary(n, k)
     k_list = k_num.split('0')
-    
-    # flag = prime(int(k_num))
     
     for kk in k_list:
         if kk == '':
